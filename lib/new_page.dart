17ebe345522b3ab86da4bd1e094cc521c6b9ea93
bottleroad <40980608+bottleroad@users.noreply.github.com>
2021-07-29 18:18:02 +0900

    Update new_page.dart

--- lib/new_page.dart
+++ lib/new_page.dart
@@ -4,8 +4,6 @@ import 'package:logindice/main.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
